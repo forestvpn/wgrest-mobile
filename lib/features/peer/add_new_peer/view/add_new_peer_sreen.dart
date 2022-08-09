f056f75d5efb96d8698bbb7f79c42fa038e75499
Stanislav Ilin <youngfrezyx@gmail.com>
2022-08-09 21:48:03 +0500

    Add router initialization

--- lib/features/peer/add_new_peer/view/add_new_peer_sreen.dart
+++ lib/features/peer/add_new_peer/view/add_new_peer_sreen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class AddNewPeerScreen extends StatelessWidget {
   const AddNewPeerScreen({Key? key}) : super(key: key);
@@ -9,7 +7,7 @@ class AddNewPeerScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Add new Peer'),
+        title: const Text('Add new Peer'),
       ),
     );
   }
