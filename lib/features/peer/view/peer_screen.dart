b461dbcbaa297ff7e12f74ba8b3758472eea3c18
Stanislav Ilin <youngfrezyx@gmail.com>
2022-10-04 02:12:17 +0600

    Update peer_screen.dart

--- lib/features/peer/view/peer_screen.dart
+++ lib/features/peer/view/peer_screen.dart
@@ -1,7 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-
-import '../../../router/router.gr.dart';
+import 'package:wgrest/router/router.dart';
 
 class PeerScreen extends StatelessWidget {
   const PeerScreen({Key? key}) : super(key: key);
